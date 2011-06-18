fddf9dae548395c3c967c94667b73665cdff5e65
Carlos Correa <razor950@gmail.com>
2011-06-18 19:47:04 -0400

    Testing Voodoo Color

--- Makefile
+++ Makefile
@@ -181,7 +181,7 @@ SUBARCH := $(shell uname -m | sed -e s/i.86/i386/ -e s/sun4u/sparc64/ \
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
 export KBUILD_BUILDHOST := $(SUBARCH)
 ARCH		?= arm
-CROSS_COMPILE	?= /home/gtg465x/CodeSourcery/Sourcery_G++_Lite/bin/arm-none-eabi-
+CROSS_COMPILE	?= /home/razor950/android/compiler/arm-2011.03/bin/arm-none-eabi-
 
 
 # Architecture as present in compile.h
--- drivers/video/samsung/s3cfb_mdnie_value.h
+++ drivers/video/samsung/s3cfb_mdnie_value.h
@@ -189,6 +189,23 @@ mDNIe_data_type mDNIe_Gallery[]=
 
 mDNIe_data_type mDNIe_Video[]= 
 {
+#ifdef CONFIG_VOODOO_MDNIE
+	// Voodoo color: mDNIe settings optimized for most video
+	// standard response curve
+	// nice sharpness filter and very light color saturation boost
+	0x0084, 0x0040,
+	0x0090, 0x0000,
+	0x0094, 0x0FFF,
+	0x0098, 0x005C,
+	0x009C, 0x0FF5,
+	0x00AC, 0x0007,
+	0x00B4, 0x0500,
+	0x00C0, 0x0400,
+	0x00C4, 0x7200,
+	0x00C8, 0x008D,
+	0x00D0, 0x00C0,
+	END_SEQ, 0x0000,
+#else
 	0x0084,0x0040, //algorithm selection+pcc + mcm
 	0x0090,0x0000, //decontour th.
 	0x0094,0x0fff, //directional th.
@@ -201,6 +218,7 @@ mDNIe_data_type mDNIe_Video[]=
 	0x00c8,0x008d, //    cr
 	0x00d0,0x0100, //    strength. 3
 	END_SEQ, 0x0000,
+#endif
 };
 
 mDNIe_data_type mDNIe_Camera[]= 
@@ -236,6 +254,25 @@ mDNIe_data_type mDNIe_Camera_Outdoor_Mode[]=
 
 mDNIe_data_type mDNIe_UI[]= 
 {
+#ifdef CONFIG_VOODOO_MDNIE
+	// Voodoo color: optimized UI mode
+	// reduce the sharpness filter radius to make it much closer
+	// to the real fuzzyness introduced by the SAMOLED Pentile pattern
+	// color saturation boost on everything is also disabled because
+	// it causes harm on stock settings (exaggerated colors)
+	0x0084, 0x0040,
+	0x0090, 0x0000,
+	0x0094, 0x0FFF,
+	0x0098, 0x005C,
+	0x009C, 0x0613,
+	0x00AC, 0x0000,
+	0x00B4, 0x0A00,
+	0x00C0, 0x0400,
+	0x00C4, 0x7200,
+	0x00C8, 0x008D,
+	0x00D0, 0x00C0,
+	END_SEQ, 0x0000,
+#else
 	0x0084,0x0040, //algorithm selection + mcm
 	0x0090,0x0000, //decontour th.
 	0x0094,0x0fff, //directional th.
@@ -249,10 +286,27 @@ mDNIe_data_type mDNIe_UI[]=
 	0x00d0,0x00c0,
 	0x0100,0x0000,
 	END_SEQ, 0x0000,
+#endif
 };
 
 mDNIe_data_type mDNIe_Video_Warm[]= 
 {
+#ifdef CONFIG_VOODOO_MDNIE_VIDEOS_ALT_PRESETS
+	// Voodoo color: high vibrance/saturation and sharpening
+	// Boost mode for videos
+	0x0084, 0x0040,
+	0x0090, 0x0000,
+	0x0094, 0x0FFF,
+	0x0098, 0x005C,
+	0x009C, 0x0FFF,
+	0x00AC, 0x0200,
+	0x00B4, 0x0800,
+	0x00C0, 0x0400,
+	0x00C4, 0x7200,
+	0x00C8, 0x008D,
+	0x00D0, 0x00C0,
+	END_SEQ, 0x0000,
+#else
 	0x0084,0x0020, //algorithm selection+pcc + mcm
 	0x0090,0x0000, //decontour th.
 	0x0094,0x0fff, //directional th.
@@ -264,6 +318,7 @@ mDNIe_data_type mDNIe_Video_Warm[]=
 	0x0138,0x7600, //cb 0
 	0x0140,0x0090, //cr 0
 	END_SEQ, 0x0000,
+#endif
 };
 
 mDNIe_data_type mDNIe_Video_WO_Mode[]= 
@@ -286,6 +341,22 @@ mDNIe_data_type mDNIe_Video_WO_Mode[]=
 
 mDNIe_data_type mDNIe_Video_Cold[]= 
 {
+#ifdef CONFIG_VOODOO_MDNIE_VIDEOS_ALT_PRESETS
+	// Voodoo color : sharpness filter at minimum, unmodified color
+	// Soft mode. Useful to counter artifacts on bad or noisy videos
+	0x0084, 0x0040,
+	0x0090, 0x0000,
+	0x0094, 0x0FFF,
+	0x0098, 0x005C,
+	0x009C, 0x0010,
+	0x00AC, 0x0000,
+	0x00B4, 0x0000,
+	0x00C0, 0x0400,
+	0x00C4, 0x7200,
+	0x00C8, 0x008D,
+	0x00D0, 0x00C0,
+	END_SEQ, 0x0000,
+#else
 	0x0084,0x0020, //algorithm selection+pcc + mcm
 	0x0090,0x0000, //decontour th.
 	0x0094,0x0fff, //directional th.
@@ -297,6 +368,7 @@ mDNIe_data_type mDNIe_Video_Cold[]=
 	0x0140,0x9400, //cb +19
 	0x0148,0x006d, //cr -19
 	END_SEQ, 0x0000,
+#endif
 };
 
 mDNIe_data_type mDNIe_Video_CO_Mode[]= 
